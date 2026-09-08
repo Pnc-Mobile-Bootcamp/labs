14a93a7d35ee1d413b23561e397cbc658b4d9e3d
Drew Fierst <drew@logicalimagination.com>
2026-09-08 14:20:36 -0400

    day 11 exercise

---
+++ Phase2/Module06/SwiftUIViews/SwiftUIViews_Starter.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUI Views
+//
+//  Module 06 Building iOS User Interfaces
+//  Lab Exercise Build Two Views
+//  
+//  SCENARIO
+//  Our mobile app needs to help customers become familiar with
+//  our products. Your task is to create two views using SwiftUI. 
+//  The first will display a list of products and the second will
+//  display details for one product.
+//  
+//  REQUIREMENTS
+//  1.  The Product class must contain the following properties:
+//          - id is an Int
+//          - name is a String
+//          - productNumber is a String
+//          - color is a String
+//          - listPrice is a Double
+//  2.  The ProductList view must use a NavigationStack to display
+//      the list of Products. The name and color should be displayed
+//  3.  The Products should be loaded via a Task. You should hard
+//      code several products into the Product loading function.
+//  4.  The application should display the ProductList view when 
+//      it first loads
+//  5.  The ProductDetails view should display all of the properties
+//      of one Product.
+//  6.  Tapping on one of the Products in the list should navigate
+//      to the details screen and also pass the selected Product
+//      to be displayed.
+//  7.  The Product class and both views should be committed to your
+//      homework repo and pushed.
+//  
